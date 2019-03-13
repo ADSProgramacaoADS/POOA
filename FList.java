eb54a77c27248b46aa84d913f13b0a7b0a99795f
ADSProgramacaoADS <48534748+ADSProgramacaoADS@users.noreply.github.com>
2019-03-13 20:27:37 -0300

    Update FList.java

--- FList.java
+++ FList.java
@@ -6,15 +6,38 @@ public abstract class FList<T> {
 	public abstract boolean isEmpty();
 	
 	public static <E> FList<E> empty(){
-		
+		return new FList<E>(){
+			public E head() {
+				throw new IllegalStateException();
+			}
+			public FList<E> tail() {
+				throw new IllegalStateException();
+			}
+			public boolean isEmpty() {
+				return true;
+			}
+		};
+	}
+	
+	public static <E> FList<E> cons(E head, FList<E> tail){
+		return new FList<E>() {
+			public E head() {
+				return head;
+			}
+			public FList<E> tail() {
+				return tail;
+			}
+			public boolean isEmpty() {
+				return false;
+			}
+		};
 	}
 	
 	public static <E> int size(FList<E> xs) {
 		if(xs.isEmpty()) {
 			return 0;
-		}else {
-			return 1 + size(xs.tail());
 		}
+		return 1 + size(xs.tail());
 	}
 	
 }
